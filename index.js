693055c1bfbb4c889cb80a6d33e84e2f3ec73cec
Alejandro E. Rendon <alejorendon@utp.edu.co>
2018-07-11 09:04:09 -0500

    Connection to DB and simple queries

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const express = require('express');
+const morgan = require('morgan');
+const mysql = require('mysql');
+
 const app = express();
+
+app.use(morgan('combined'));
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'test'
+});
 
 app.get('/', (req, res) => {
   console.log('Responding to root route');
@@ -7,11 +19,27 @@ app.get('/', (req, res) => {
 });
 
 app.get('/users', (req, res) => {
-  const user1 = [
-    { firstName: "Stephen", lastName: "Curry" },
-    { firstName: "Stephen", lastName: "Curry" } 
-  ];
-  res.status(200).json(user1);
+  connection.query("SELECT * FROM user", (err, rows, fields) => {
+    console.log('I think we fetched users');
+    res.status(200).json(rows);
+  });
+});
+
+app.get('/users/:id', (req, res) => {
+  const userId = req.params.id;
+  const queryString = "SELECT * FROM user WHERE user_id = ?";
+
+  connection.query(queryString, [userId], (err, rows, fields) => {
+    if (err) {
+      console.log('Failed to query for users');
+      res.end();
+      return;
+    }
+    console.log('I think we fetched users');
+    res.status(200).json(rows);
+  });
+
+  // res.end();
 });
 
 app.listen(3000, () => {
